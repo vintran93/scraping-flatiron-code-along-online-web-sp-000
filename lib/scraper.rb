d85b315c6b3f331590e657375dd5cd3343a84794
Vincent Tran <vincentuong@yahoo.com>
2021-07-01 04:10:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,22 +7,15 @@ require_relative './course.rb'
 class Scraper
  
   def get_page
-    
-     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-     
-    #doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-  
-    
+ 
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
     #doc.css(".post").each do |post|
-     # course = Course.new
+      #course = Course.new
       #course.title = post.css("h2").text
       #course.schedule = post.css(".date").text
       #course.description = post.css("p").text
     #end
-      
   end
  
-    
 end
- 
-Scraper.new.get_page
